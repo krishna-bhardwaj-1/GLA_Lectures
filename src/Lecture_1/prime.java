d874e468f6d75a44dafa4d7abefaab1966575f9b
krishna-bhardwaj-1 <krishnabhardwaj5427912@gmail.com>
2024-08-09 17:28:05 +0530

    Loop demo

--- src/Lecture_1/prime.java
+++ src/Lecture_1/prime.java
@@ -2,6 +2,7 @@ package Lecture_1;
 
 public class prime {
     public static void main(String[] args) {
+        System.out.println("Prime numbers from 1 to 100.");
         for(int a=1;a<100;a++) {
             int c=0;
             for(int i=1;i<=(a/2);i++) {
