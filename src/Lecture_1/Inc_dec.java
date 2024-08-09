3b8c6ffae3b0599619f2802c8c669645ceec5dbb
krishna-bhardwaj-1 <krishnabhardwaj5427912@gmail.com>
2024-08-09 17:13:23 +0530

    Loop demo

---
+++ src/Lecture_1/Inc_dec.java
@@ -0,0 +1,9 @@
+package Lecture_1;
+
+public class Inc_dec {
+    public static void main(String[] args) {
+        int a=9;
+        int x=a++ - ++a - 7 + --a + a--;
+        System.out.println(x);
+    }
+}
