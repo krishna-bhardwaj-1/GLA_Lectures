45606a47c0f02c75b5a1ac1feb558b4aeb0b4e22
krishna-bhardwaj-1 <krishnabhardwaj5427912@gmail.com>
2024-08-09 22:56:46 +0530

    Loop demo

---
+++ src/Pattern_printing/Q_4.java
@@ -0,0 +1,13 @@
+package Pattern_printing;
+
+public class Q_4 {
+    public static void main(String[] args) {
+        /*
+                 *
+               * *
+             * * *
+           * * * *
+         * * * * *
+         */
+    }
+}
