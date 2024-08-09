a508fd13b6cbdde1fa52b5d00a0bc003181a1684
krishna-bhardwaj-1 <krishnabhardwaj5427912@gmail.com>
2024-08-09 17:32:32 +0530

    Loop demo

--- src/Lecture_1/Inc_dec.java
+++ src/Lecture_1/Inc_dec.java
@@ -2,6 +2,7 @@ package Lecture_1;
 
 public class Inc_dec {
     public static void main(String[] args) {
+        System.out.println("By using the concept of pre-increment and pre-decrement.");
         int a=9;
         int x=a++ - ++a - 7 + --a + a--;
         System.out.println(x);
