f0a3e074c72c459fcf2633c4d2b8da6195204b74
krishna-bhardwaj-1 <krishnabhardwaj5427912@gmail.com>
2024-08-10 20:08:20 +0530

    pyramid pattern

--- src/Pattern_printing/Q_12.java
+++ src/Pattern_printing/Q_12.java
@@ -2,33 +2,34 @@ package Pattern_printing;
 
 import java.util.Scanner;
 
-public class Q_11 {
+public class Q_12 {
     public static void main(String[] args) {
         /*
                 *
-              *   *
-            *   *   *
-          *   *   *   *
-        *   *   *   *   *
+              * ! *
+            * ! * ! *
+          * ! * ! * ! *
+         * ! * ! * ! * ! *
          */
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
         int i,j,k;
         for( i=1;i<=n;i++){
             for(j=i;j<n;j++){
-                System.out.print("  ");
+                System.out.print("   ");
             }
             for(k=1;k<=2*i-1;k++){
                 if(i%2!=0) {
-                    if ((i + k) % 2 != 0) System.out.print("  ");
-                    else System.out.print("* ");
+                    if ((i + k) % 2 != 0) System.out.print(" ! ");
+                    else System.out.print(" * ");
                 }
                 if(i%2==0){
-                    if((i+k)%2==0) System.out.print("  ");
-                    else System.out.print("* ");
+                    if((i+k)%2==0) System.out.print(" ! ");
+                    else System.out.print(" * ");
                 }
             }
             System.out.println();
         }
+
     }
 }
