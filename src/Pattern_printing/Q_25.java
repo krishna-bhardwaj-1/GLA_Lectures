6de374932980a6059bec8e0841f7efadc278fee2
krishna-bhardwaj-1 <krishnabhardwaj5427912@gmail.com>
2024-08-13 16:52:36 +0530

    patterns

--- src/Pattern_printing/Q_25.java
+++ src/Pattern_printing/Q_25.java
@@ -5,24 +5,36 @@ import java.util.Scanner;
 public class Q_25 {
     public static void main(String[] args) {
         /*
-                    1
-                  2 2 2
-                3 3 3 3 3
-              4 4 4 4 4 4 4
-            5 5 5 5 5 5 5 5 5
+                             1
+                         2   3   4
+                     5   6   7   8   9
+                 10  11  12  13  14  15  16
+             17  18  19  20  21  22  23  24  25
          */
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int c=1;
         for(int i=1;i<=n;i++){
-            for(int j=i;j<n;j++){
-                System.out.print("     ");
+            if(c<10) {
+                for (int j = i; j < n; j++) {
+                    System.out.print("    ");
+                }
+                for (int k = 1; k <= (2 * i - 1); k++) {
+                    System.out.print(" " + c + "  ");
+                    c++;
+                }
+                System.out.println();
             }
-            for(int k=1;k<=(2*i-1);k++) {
-                System.out.print("  "+c + " ");
-                c++;
+            else{
+                for (int j = i; j < n; j++) {
+                    System.out.print("    ");
+                }
+                for (int k = 1; k <= (2 * i - 1); k++) {
+                    System.out.print(" " + c + " ");
+                    c++;
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
