dbeecf960fa02448456d53ff2c08889651426cf4
krishna-bhardwaj-1 <krishnabhardwaj5427912@gmail.com>
2024-08-18 17:51:20 +0530

    Assignment coding blocks

--- src/Assignment_1/Pattern_inverted_hour_glass.java
+++ src/Assignment_1/Pattern_inverted_hour_glass.java
@@ -6,12 +6,11 @@ public class Pattern_inverted_hour_glass {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int row=1;
+        int row=2*n+1;
         int star=1;
-        int st=5;
         int spaces=2*n-1;
-        while(row<=2*n+1) {
-            int i = 1, j = 1,k=5;
+        while(row>=1) {
+            int i = 1, j = 1;
             //star
             int c = 5;
             while (i <= star) {
@@ -25,11 +24,13 @@ public class Pattern_inverted_hour_glass {
                 j++;
             }
             //star
-
+            for(int q=n;q>=1;q--){
+                System.out.print(q);
+            }
             if(row<=n){ star++; spaces-=2;}
             else{ star--; spaces+=2;}
             System.out.println();
-            row++;
+            row--;
         }
     }
 }
