98af75abd68b1262923fec94e99250966e30fe99
krishna-bhardwaj-1 <krishnabhardwaj5427912@gmail.com>
2024-09-02 22:20:21 +0530

    Assignment 6489

---
+++ src/Assignment_6498/print_series.java
@@ -0,0 +1,17 @@
+package Assignment_6498;
+
+import java.util.Scanner;
+
+public class print_series {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int i=1;
+        while(i<=a){
+            if((3*i+2)%4!=0) System.out.println(3*i+2);
+            else a++;
+            i++;
+        }
+    }
+}
