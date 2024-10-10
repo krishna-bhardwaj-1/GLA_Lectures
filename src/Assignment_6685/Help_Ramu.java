1d6886b0ba0e65204368f7ca5fcd68a461ddac0b
krishna-bhardwaj-1 <krishnabhardwaj5427912@gmail.com>
2024-10-10 23:54:04 +0530

    array

---
+++ src/Assignment_6685/Help_Ramu.java
@@ -0,0 +1,28 @@
+package Assignment_6685;
+
+import java.util.Scanner;
+
+public class Help_Ramu {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int p=sc.nextInt();
+        while(p>0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            System.out.println(correct(x,y,arr));
+            p--;
+        }
+    }
+    public static boolean correct(int x, int y, int[] arr){
+        int c=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<=x) c++;
+        }
+        if(c==y) return true;
+        else return false;
+    }
+}
--- src/Assignment_6685/Pair_of_roses.java
+++ src/Assignment_6685/Pair_of_roses.java
@@ -13,24 +13,22 @@ public class Pair_of_roses {
                 arr[i] = sc.nextInt();
             }
             Sort(arr);
-
             int tar = sc.nextInt();
             int max=tar;
             int a=0,b=0;
             for (int i = 0; i < n; i++) {
                 for (int j = i + 1; j < n; j++) {
                     if (arr[i] + arr[j] == tar){
-                        if(max>arr[i]-arr[j]) {
+                        if(max>arr[j]-arr[i]) {
                             a = arr[i];
                             b = arr[j];
-                            max = Math.min(max, (arr[i] - arr[j]));
+                            max = arr[j]-arr[i];
                         }
                     }
                 }
             }
-            System.out.print(a+" "+b);
+            System.out.print("Deepak should buy roses whose prices are "+a+" and "+b+".");
             turn--;
-
         }
     }
     public static void Sort(int[] arr){
